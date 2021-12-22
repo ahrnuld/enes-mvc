1d6c015027e44b2fefe519aeea3e48d15274cc63
Mark-Inholland <mark.dehaan@inholland.nl>
2021-12-22 11:17:27 +0100

    first commit

---
+++ app/public/index.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+require __DIR__ . '/../patternrouter.php';
+
+$uri = trim($_SERVER['REQUEST_URI'], '/');
+
+$router = new PatternRouter();
+$router->route($uri);
